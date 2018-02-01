fead8cdaa580b51b2dfa6de7b187ccc9baf91923
zhangshuang <shuang199054@163.com>
2018-02-01 11:32:13 +0800

    add os/exec demo

--- common-package/exec.go
+++ common-package/exec.go
@@ -1,5 +1,58 @@
 package main
 
+import (
+	"os/exec"
+	"log"
+	"fmt"
+	"encoding/json"
+)
+
+type Result struct {
+	Code int `json:"errcode"`
+	Msg string `json:"errmsg"`
+}
+
 func main() {
+	//send dingding robot message
+	r, err := exec.Command("bash", "-c", `curl https://oapi.dingtalk.com/robot/send?access_token=e78992df5c1aa619bcf6b83044dc341d122fc \
+        -H 'Content-Type: application/json' \
+        -d '
+          {"msgtype": "text",
+            "text": {
+                "content": "test message, pleat ignore these"
+             }
+          }'`).Output()
+
+    if err != nil {
+    	fmt.Println(err)
+    	return
+	}
+
+	// convert json result to struct
+    result := Result{}
+    err = json.Unmarshal(r, &result)
+    if err != nil {
+    	fmt.Println("json decode failed: ", err)
+	}
+
+	fmt.Printf("%+v\n", result)
+	if result.Code != 0 {
+		fmt.Println(result.Msg)
+	}
+
+	//find the command path
+	path, err := exec.LookPath("curl")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(path)
+
+	// call date print current date
+	out, err := exec.Command("date", `+"%Y-%m-%d"`).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The date is %s\n", string(out))
 
 }
