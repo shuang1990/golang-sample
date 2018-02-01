06bc08b9ba1e399bf9465d7bf42159ec4a2531c4
zhangshuang <shuang199054@163.com>
2018-02-01 17:22:31 +0800

    add flag demo

--- common-package/flag.go
+++ common-package/flag.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+var (
+	config string
+)
+
+func main() {
+	flag.StringVar(&config, "c", "/etc/nginx/nginx.conf", "please input the nginx config")
+	addr := flag.String("addr", "127.0.0.1", "http service address")
+	port := flag.Int("port", 8080, "http service listen port")
+
+	flag.Parse()
+
+	fmt.Printf("addr : %s\n", *addr)
+	fmt.Printf("config file : %s\n", config)
+	fmt.Printf("port: %d\n", *port)
 }
